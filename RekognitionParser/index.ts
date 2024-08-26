afde4b440d0af908be3e7131432c8478606a4794
Mario Olivera <mario.olivera96@gmail..com>
2024-08-26 12:02:10 -0300

    rm code viejo

--- RekognitionParser/index.ts
+++
@@ -1,8 +0,0 @@
-import { RekognitionReader } from "./RekognitionReader";
-
-/*
-const my_test = new RekognitionReader();
-const data_rekognition = my_test.process();
-
-console.log("data_rekognition", data_rekognition);
-*/
